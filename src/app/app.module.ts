fe031a71cfec54df40e07e39ad1930598a796517
MajdAZD <90031294+MajdAZD@users.noreply.github.com>
2021-09-20 16:00:58 +0200

    ProjectSWAPI

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { PlanetDetailsComponent } from './planet-list/planet-details/planet-details.component';
+import { PlanetListComponent } from './planet-list/planet-list.component';
+import { PlanetComponent } from './planet-list/planet/planet.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PlanetListComponent,
+    PlanetComponent,
+    PlanetDetailsComponent,
+  ],
+  imports: [HttpClientModule, BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
