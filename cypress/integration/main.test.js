d09d52b94379a7e54cdcce2cca989f72c35dfba0
Majd Abazid <majd.abazid@i40mc.de>
2021-10-08 18:14:40 +0200

    Version1

---
+++ cypress/integration/main.test.js
@@ -0,0 +1,27 @@
+describe("homepage",() => {
+  beforeEach(()=>{
+    cy.fixture('planets.json').as("planetsJSON");//simulating the response of the http request. access the file
+    cy.server(); //simulating the server
+    cy.route('https://swapi.dev/api/planets', "@planetsJSON").as("planets");/*linking our planets.json to http request*/
+    cy.visit('/');
+    //cy.intercept('api/planets', "@planetsJSON").as("planets");TODO migrate from server/route to intercept
+  });
+
+  it('should display a list of 60 planets', ()=>{
+
+  cy.contains("Galaxy");
+
+  cy.wait('@planets');
+  cy.wait// wait for the response to be simulated and the data are displayed on the screen
+
+  // expect(status).to.eq(200); // TODO try use with wait => then expect!
+  cy.get("app-planet").should("have.length", 60);// look for particular element on the page
+  });
+
+  it('should display planet details', ()=>{
+  cy.wait('@planets');
+  cy.get("app-planet").click({multiple: true});
+  //cy.get('app-planet-details .list-group-item').its('length').should('be.gt', 8);
+  cy.get('app-planet-details').should("have.length", 1);
+  });
+});
